710cbdb3361dee58913eac2bfdb58e8c73cc5c1e
hydra1245 <down579@naver.com>
2014-09-20 16:45:03 +0900

    수정

--- MudGameBIT/src/game/server/MudgameServer.java
+++ MudGameBIT/src/game/server/MudgameServer.java
@@ -1,5 +1,8 @@
 package game.server;
 
 public class MudgameServer {
-
+	public static void main(String[] args) {
+		Player py = new Player();
+		py.skillAttack(0);
+	}
 }
--- MudGameBIT/src/game/server/Player.java
+++ MudGameBIT/src/game/server/Player.java
@@ -10,11 +10,13 @@ public class Player extends Unit {
 	public void useItem(int sel){
 		
 	}
-	public void move(){
-		
+	public void move(Unit u){
+		u.distance++;
+		System.out.println("이동합니다.");
 	}
 	public void skillAttack(int sel){
-		
+		int damage = (int)(Math.random() * (20-10+1))+10;
+		System.out.println(damage);
 	}
 	
 }
